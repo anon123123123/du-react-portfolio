a632321d1cade4c33a4ffd33e2a129132a872ae7
anon123123123 <cditodavis96@gmail.com>
2022-05-28 10:39:17 -0600

    pages fix?

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import '../styles/Header.css';
 
 function Header() {
   return (
-    
+
     <header className="header">
       <h1>Anon McAnonerson's Portfolio</h1>
-      <img id='profile-pic' src='./images/exorcist.jpeg'></img>
+      <img id='profile-picture' src='./images/exorcist.jpeg'></img>
     </header>
   );
 }
